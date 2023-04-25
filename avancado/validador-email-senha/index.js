5b058091f359ebc9d85f6eb94d1f2afeb84734a7
Guilherme Henrique <103789281+Guihenrique62@users.noreply.github.com>
2023-04-25 14:09:49 -0300

    validador

---
+++ avancado/validador-email-senha/index.js
@@ -0,0 +1,10 @@
+document.getElementById('username').addEventListener('keydown', (key)=>{
+    
+})
+
+const inputEmail = document.getElementById('email')
+inputEmail.addEventListener('keyup',()=>{
+    const inputValue = inputEmail.value
+    
+    
+})
