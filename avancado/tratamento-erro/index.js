037cd204cd56f7a314546acec82fcd05edea1748
Guilherme Henrique <103789281+Guihenrique62@users.noreply.github.com>
2023-04-27 14:29:10 -0300

    gg

--- avancado/tratamento-erro/index.js
+++ avancado/tratamento-erro/index.js
@@ -10,7 +10,7 @@ function sum(a,b){
 try{
     console.log(sum(2,9))
     console.log(sum(true,14))
-    // console.log(sum(undefined,22))
+    console.log(sum(undefined,22))
     console.log(sum(18,'0'))
     console.log(sum(39,null))
     // console.log(sum(13,"zero"))
