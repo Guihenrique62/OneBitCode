9174c596bd15a0ef91a6cb7c53025810baa24dfc
Guilherme Henrique <103789281+Guihenrique62@users.noreply.github.com>
2023-06-04 22:40:06 -0300

    typescript

---
+++ typeScript/criar-tipos/src/index.ts
@@ -0,0 +1,13 @@
+let option: 'yes' | 'no' 
+
+option = 'yes'
+
+//alaias
+type Planet = 'Terra' | 'Marte' | 'Jupiter' | 'Saturno' | 'Urano' | 'Netuno' | 'Plutao'
+
+let planet: Planet
+let homeplanet: Planet
+
+if(planet === 'Jupiter'){
+   console.log('estamos em jupiter')
+}
